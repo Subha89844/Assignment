c6510052632a6c994da7dc29fd5edd440d852f8d
Subhashree Rout <100186534+Subha89844@users.noreply.github.com>
2026-04-05 21:07:14 +0530

    Add files via upload

---
+++ JavaSeleniumProject/src/main/java/Project/JavaSeleniumProject/App.java
@@ -0,0 +1,10 @@
+package Project.JavaSeleniumProject;
+
+/**
+ * Hello world!
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
